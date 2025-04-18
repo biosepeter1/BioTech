7cba13dab5d8301a4d5b40b5e748fdb9906280b5
unknown <biosejohn@gmail.com>
2025-04-18 14:36:57 +0100

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import session from "express-session";
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
